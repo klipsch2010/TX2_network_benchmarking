cedd105d744108f2f9d580b814b523a5e18f841f
klipsch2010 <archdukeobvious@gmail.com>
2018-11-18 03:54:44 -0700

    googlnet run code added

---
+++ alex-googlenet/build/segnet-camera/CMakeFiles/segnet-camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segnet-camera.dir/segnet-camera.cpp.o"
+  "../aarch64/bin/segnet-camera.pdb"
+  "../aarch64/bin/segnet-camera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segnet-camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ alex-googlenet/build/segnet-console/CMakeFiles/segnet-console.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/segnet-console.dir/segnet-console.cpp.o"
+  "../aarch64/bin/segnet-console.pdb"
+  "../aarch64/bin/segnet-console"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/segnet-console.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
